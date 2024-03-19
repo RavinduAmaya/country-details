c99b2be14adf6d0effb1a50df4e58a3b8a866c5f
Ravindu.Shanthalal <ravindu.shanthalal@LKCOL-FT7TKW7WKH.local>
2024-03-19 09:54:56 +0530

    project setup done

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <CountryList/> -->
+    <!-- <CountryPage/> -->
+    <!-- <Map/> -->
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  modules: [
+    'nuxt3-leaflet'
+  ]
+})
