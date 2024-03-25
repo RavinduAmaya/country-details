c9ab5f467af751de6d7aceb96ff87cdfc33ef091
RavinduAmaya <ravindu1997amaya@gmail.com>
2024-03-25 14:05:31 +0530

    map component added

---
+++ components/Map/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="map-container">
+    <LMap ref="map" :zoom="zoom" :center="[latitude, longtitude]">
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+      <LMarker :lat-lng="[latitude, longtitude]"></LMarker>
+    </LMap>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Map",
+  data: () => ({
+    zoom: ref(2),
+  }),
+  props: {
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longtitude: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+.map-container {
+  height: 45vh;
+  width: 30vw;
+}
+</style>
