4e3406f22a1fb2278c7b538a03826d20a53434a3
RavinduAmaya <ravindu1997amaya@gmail.com>
2024-03-25 22:41:31 +0530

    homepage component added with index page

---
+++ components/HomePage/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-page__title">Choose Your Country</div>
+  <hr/>
+  <CountryList />
+</template>
+
+<script>
+import CountryList from "../../pages/CountryList.vue";
+
+export default {
+  components: { CountryList },
+};
+</script>
+
+<style>
+.home-page__title {
+  text-align: center;
+  margin-top: 25px;
+  font-size: 35px;
+  font-weight: bold;
+  color: grey;
+}
+hr{
+  width: 90%;
+}
+</style>
